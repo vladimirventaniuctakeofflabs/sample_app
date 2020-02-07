a85e738cb632cf561fdb023d6c79b2772394d368
Vladimir Ventaniuc <vladimir.ventaniuc@takeofflabs.com>
2020-02-07 17:40:21 +0200

    Add user following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -33,18 +33,5 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 
-  test "micropost sidebar count" do
-    log_in_as(@user)
-    get root_path
-    assert_match "#{FILL_IN} microposts", response.body
-    # User with zero microposts
-    other_user = users(:malory)
-    log_in_as(other_user)
-    get root_path
-    assert_match "0 microposts", response.body
-    other_user.microposts.create!(content: "A micropost")
-    get root_path
-    assert_match FILL_IN, response.body
-  end
-  
+
 end
