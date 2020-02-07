c285db274c08b10c5da0bca7386bc18087da1855
Vladimir Ventaniuc <vladimir.ventaniuc@takeofflabs.com>
2020-02-07 15:21:41 +0200

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'faker',          '2.10.1'
 gem 'will_paginate',           '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
 
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -60,6 +63,7 @@ end
 
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   root 'static_pages#home'
 
-  get '/signup',     to: 'users#new'
+  get '/signup',   to: 'users#new'
   get '/home',     to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   
 end
